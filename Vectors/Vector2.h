012cc59441e90001e0f7a6d8106dfee4fc7ad77b
kdestroyer007 <kdestroyer664@gmail.com>
2026-06-03 22:16:11 +0530

    Add files via upload

---
+++ Vectors/Vector2.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Vec2{
+    float x;
+    float y;
+};
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Vectors/V_alzebra.h"
+
+using namespace std;
+
+int main(){
+    Vec2 a = {1,2};
+    Vec2 b = {5,6};
+    Vec2 c = a+b ;
+    cout << c << endl;
+    
+}
